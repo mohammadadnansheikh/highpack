20afacb0fe3f8fc8ef12b9a751276f203d8a64df
ADNAN <adnansheikh4ualways@gmail.com>
2023-03-07 11:06:58 +0530

    whatsapp

--- components/WhatsApp.js
+++ components/WhatsApp.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 const WhatsApp = () => {
   return (
     <div className="bg-[#25D366] fixed z-[100] text-white p-[.8rem] sm:p-[1rem] rounded-full rounded-2 text-[2rem] sm:text-[3rem] md:text-[3rem] right-[2rem] bottom-[2rem] md:right-[4rem] md:bottom-[4rem] cursor-pointer hover:bg-[#075E54]  duration-300">
-       <Link href="https://wa.me/6206941545"><BsWhatsapp/></Link>
+       <Link href="https://wa.me/+916206941545"><BsWhatsapp/></Link>
        
     </div>
   )
